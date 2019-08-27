6d3fb5d864a6358df8ee1346d0e2cccc0503f3ed
Howie Katz <howard.katz@flashingmachine.com>
2019-08-26 21:34:28 -0700

    working with bone choice and hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import PageWithScene from './PageWithScene';
+import AvatarScene from './AvatarScene';
 
 function App() {
+  const [boneNum, setBoneNum] = useState(14);
   return (
-      <PageWithScene />
+      <div>
+      <AvatarScene boneNum={boneNum}/>
+      <button type="button" onClick={() =>setBoneNum(boneNum-1)}>Less</button>
+      <span>{boneNum}</span>
+      <button type="button" onClick={() =>setBoneNum(boneNum+1)}>More</button>
+      </div>
   );
 }
 
---
+++ src/AvatarScene.js
@@ -0,0 +1,95 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+  Engine,
+  Scene,
+  FreeCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  SceneLoader,
+  ArcRotateCamera
+} from "babylonjs";
+
+import avatar from "./assets/Dude.json";
+
+const AvatarScene = ({ boneNum }) => {
+  // const [scene, setScene] = useState(null);
+  const [engine, setEngine] = useState(null);
+  const [skeleton, setSkeleton] = useState(null);
+  const [previousBone, setPreviousBone] = useState(null);
+  const canvasRef = useRef(null);
+
+
+  useEffect(() => {
+    if (skeleton) {
+      if (previousBone) {
+        previousBone.setScale(new Vector3(1, 1, 1));
+      }
+
+      let bone = skeleton.bones[boneNum];
+      if (!bone) return;
+      bone.setScale(new Vector3(2, 2, 2));
+      setPreviousBone(bone);
+    }
+  }, [skeleton, boneNum, previousBone]);
+
+  useEffect(() => {
+    if (engine) return; // only do once
+    const canvas = canvasRef.current;
+    const e = new Engine(canvas, true);
+    setEngine(e);
+
+    const scene = new Scene(e);
+    const camera = new ArcRotateCamera(
+      "camera",
+      1,
+      1,
+      140,
+      Vector3.Zero(),
+      scene
+    );
+
+    camera.setTarget(new Vector3(0, 40, 0));
+
+    camera.attachControl(canvas, true);
+
+    const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+
+    light.intensity = 0.7;
+    SceneLoader.ImportMesh(
+      "",
+      "",
+      `data:${JSON.stringify(avatar)}`,
+      scene,
+      function(newMeshes, particleSystems, skeletons) {
+        // mesh = newMeshes[0];
+        setSkeleton(skeletons[0]);
+      }
+    );
+    e.runRenderLoop(() => {
+      if (scene) {
+        scene.render();
+      }
+    });
+
+    const onResizeWindow = () => {
+      if (engine) {
+        engine.resize();
+      }
+    };
+  
+    window.addEventListener("resize",  onResizeWindow);
+    return () => {
+      window.removeEventListener("resize", onResizeWindow);
+    };
+  }, [engine]);
+
+  const opts = {
+    width: 1000,
+    height: 800
+  };
+
+  return <canvas {...opts} ref={canvasRef} />;
+};
+
+export default AvatarScene;
