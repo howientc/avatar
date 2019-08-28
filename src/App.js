995d367f017f7fe8f1d15ddc01ec9b7eb01b3791
Howie Katz <howard.katz@flashingmachine.com>
2019-08-28 01:22:53 -0700

    generating bone map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import _ from 'lodash';
+import { invert } from 'lodash';
 import { Vector3, Quaternion } from "@babylonjs/core/Maths/math";
 
 import mockSensors from './mock/mockSensors';
@@ -10,7 +10,7 @@ import AvatarScene from './AvatarScene';
 
 
 // These come from Unreal and correspond to boneType in database
-const boneType = {
+export const boneType = {
   0: 'Spine',
   1: 'Hand_L',
   2: 'Elbow_L',
@@ -33,23 +33,24 @@ const boneType = {
   19: 'Ankle_R',
 };
 
-const boneName = _.invert(boneType);
+const boneName = invert(boneType);
 
 const BONE_MAP = {
-  // [boneName['Spine']] : 2,
-  [boneName['Hand_L']] : 10,
-  [boneName['Elbow_L']] : 9,
-  [boneName['Hand_R']] : 32,
-  [boneName['Elbow_R']] : 31,
+  [boneName['Shoulder_L']] : 6,
+  [boneName['Arm_L']] : 7,
+  [boneName['Elbow_L']] : 10,
+  [boneName['Hand_L']] : 12 ,
+
 //hips?
 //head?
+  // [boneName['Spine']] : 2,
   // [boneName['Spine2']] : 4,
   // [boneName['Spine1']] : 3,
 // neck?  
-  [boneName['Shoulder_L']] : 6,
   [boneName['Shoulder_R']] : 29,
-  [boneName['Arm_L']] : 8,
   [boneName['Arm_R']] : 30,
+  [boneName['Elbow_R']] : 33,
+  [boneName['Hand_R']] : 35,
 };
 
 function App() {
@@ -57,26 +58,24 @@ function App() {
   const incrementBone = useCallback(() => setBoneNum(boneNum => boneNum + 1), []);
   const decrementBone = useCallback(() => setBoneNum(boneNum => boneNum - 1), []);
   const [boneInfo, setBoneInfo] = useState(null);
+  const [paused, setPaused] = useState(true);
   if (!boneInfo) {
     mockSensors(({ Skeletal }) => {
       const b = [];
-      console.log('sssss')
       Skeletal.forEach(bone => {
         const {QX, QY, QZ, QW, PX, PY, PZ, ID } = bone;
-        const boneId = BONE_MAP[ID];
-        if (boneId) {
+        // const boneId = BONE_MAP[ID];
           let q = new Quaternion(QX / 1000 , QY / 1000, QZ / 1000, QW / 1000);
           const p  = new Vector3(PX / 1000, PY/1000, PZ/1000);
-          // q = Quaternion.Inverse(q);
-          b.push( { boneId, p, q });
-        }
+          b.push( { ID, p, q });
       });
       setBoneInfo(b);
     });
   }
   return (
       <div>
-      <AvatarScene boneInfo={boneInfo} boneNum={boneNum}/>
+      <AvatarScene boneInfo={boneInfo} boneNum={boneNum} paused={paused}/>
+      <button type="button" onClick={()=>setPaused(!paused)}>{paused ? "resume" : "pause"}</button>
       <button type="button" onClick={decrementBone}>Less</button>
       <span>{boneNum}</span>
       <button type="button" onClick={incrementBone}>More</button>
--- src/AvatarScene.js
+++ src/AvatarScene.js
@@ -2,48 +2,55 @@ import React, { useRef, useState, useEffect } from "react";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
 import { SceneLoader } from "@babylonjs/core";
+import { SphereBuilder } from "@babylonjs/core/Meshes/Builders/sphereBuilder";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import {boneType} from './App';
+import { findIndex } from 'lodash';
 // import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
 // import avatar from "./assets/Dude.json";
 // import avatar from "./assets/AvatarMVI_Female_Rig.json";
 // import avatar from "./assets/AvatarMVI_Rig.json";
 import avatar from './assets/AvatarMVI_Female_Rig_TestAnim';
-const AvatarScene = ({ boneInfo, boneNum }) => {
+
+const boneMap = {};
+const buildBoneMap = (bones) => {
+  Object.entries(boneType).forEach(([ix,name]) => {
+    const avatarBone = findIndex(bones, b=> (name === b.name || name+'_Skin' === b.name));
+    if (avatarBone !== -1) boneMap[ix] = avatarBone;
+  });
+  delete boneMap[0];
+  delete boneMap[5];
+  delete boneMap[7];
+  delete boneMap[8];
+  console.log(boneMap);
+}
+
+const AvatarScene = ({ boneInfo, boneNum, paused=false }) => {
   // const [scene, setScene] = useState(null);
   const [engine, setEngine] = useState(null);
   const [skeleton, setSkeleton] = useState(null);
-  const [previousBone, setPreviousBone] = useState(null);
+  const [mesh, setMesh] = useState(null);
   const canvasRef = useRef(null);
+  const [marker, setMarker] = useState(null);
 
-  // useEffect(() => {
-  //   if (skeleton) {
-  //     if (previousBone) {
-  //       console.log('setting previous back')
-  //       previousBone.setScale(new Vector3(1, 1, 1));
-  //     }
-
-  //     let bone = skeleton.bones[boneNum];
-  //     if (!bone) {
-  //       return;
-  //     }
-  //     bone.setScale(new Vector3(2, 2, 2));
-  //     setPreviousBone(bone);
-  //   }
-  // }, [skeleton, boneNum, previousBone]);
-
-  useEffect(() => {
-      if (skeleton && boneInfo) {
+  if (skeleton) {
+    let bone = skeleton.bones[boneNum];
+    marker.attachToBone(bone, mesh);
+    if (!paused && boneInfo) {
       // console.log(boneInfo)
-      boneInfo.forEach( ({boneId, p, q } )=> {
-        let bone = skeleton.bones[boneId];
-        bone.rotationQuaternion = q;
+      boneInfo.forEach( ({ID, p, q } )=> {
+        const boneId = boneMap[ID];
+        if (boneId) {
+          let bone = skeleton.bones[boneId];
+          bone.rotationQuaternion = q;
+        }
         // bone.position = p; 
       })
     }
-  }, [skeleton, boneInfo]);
+  }
 
   useEffect(() => {
     if (engine) return; // only do once
@@ -56,26 +63,28 @@ const AvatarScene = ({ boneInfo, boneNum }) => {
       "camera",
       1,
       1,
-      140,
+      240,
       Vector3.Zero(),
       scene
     );
 
-    camera.setTarget(new Vector3(0, 40, 0));
-
+    camera.setTarget(new Vector3(0, 90, 0));
     camera.attachControl(canvas, true);
-
     const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-
     light.intensity = 0.7;
+
+    setMarker(SphereBuilder.CreateSphere("marker", {diameter: 15}, scene));
     SceneLoader.ImportMesh(
       "",
       "",
       `data:${JSON.stringify(avatar)}`,
       scene,
-      function(newMeshes, particleSystems, skeletons) {
-        // mesh = newMeshes[0];
+      (newMeshes, particleSystems, skeletons) => {
+        console.log(newMeshes, skeletons)
+        buildBoneMap(skeletons[0].bones);
+        setMesh(newMeshes[0]);
         setSkeleton(skeletons[0]);
+
       }
     );
     e.runRenderLoop(() => {
